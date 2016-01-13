16f461e472a37ef20b19cd8524187759953de12c
Thu Linh Nguyen <kissa101295@gmail.com>
2016-01-13 17:29:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 	var userAnswer=prompt('Enter a number below!');
-var xNumber = parseInt(userAnswer);
+
 	var FizzBuzz = function (x){
-		x = + userAnswer;
-		for(i=1; i<= x; i++) {
+		var convertNumber = +userAnswer;
+		var xNumber = parseInt(userAnswer);
+		for(i=1; i<= xNumber; i++) {
 	if (i % 3 === 0 && i % 5 === 0) {
         console.log("fizzbuzz");
 	}
@@ -16,7 +17,14 @@ var xNumber = parseInt(userAnswer);
 		console.log(i);
 	}
 } 
-	}	
-
+	};	
+if(isNaN(convertNumber)){
+	prompt("Please enter a number only!");
+}
+else if(xNumber % 1 !== 0)
+{
+	prompt("Please enter a whole number only!");
+}
+else{
 FizzBuzz(userAnswer);
-
+};
